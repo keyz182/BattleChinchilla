a465ea41dae80a2dd0a05df5f1252010b3356a70
Kieran David Evans <keyz182@gmail.com>
2025-06-19 11:29:55 +0100

    Cookie cutter initial commit

---
+++ 1.5/Source/BattleChinchilla/BattleChinchillaDefOf.cs
@@ -0,0 +1,14 @@
+﻿using RimWorld;
+using Verse;
+
+namespace BattleChinchilla;
+
+[DefOf]
+public static class BattleChinchillaDefOf
+{
+    // Remember to annotate any Defs that require a DLC as needed e.g.
+    // [MayRequireBiotech]
+    // public static GeneDef YourPrefix_YourGeneDefName;
+    
+    static BattleChinchillaDefOf() => DefOfHelper.EnsureInitializedInCtor(typeof(BattleChinchillaDefOf));
+}
---
+++ 1.5/Source/BattleChinchilla/BattleChinchillaMod.cs
@@ -0,0 +1,34 @@
+﻿using Verse;
+using UnityEngine;
+using HarmonyLib;
+
+namespace BattleChinchilla;
+
+public class BattleChinchillaMod : Mod
+{
+    public static Settings settings;
+
+    public BattleChinchillaMod(ModContentPack content) : base(content)
+    {
+        Log.Message("Hello world from BattleChinchilla");
+
+        // initialize settings
+        settings = GetSettings<Settings>();
+#if DEBUG
+        Harmony.DEBUG = true;
+#endif
+        Harmony harmony = new Harmony("keyz182.rimworld.BattleChinchilla.main");	
+        harmony.PatchAll();
+    }
+
+    public override void DoSettingsWindowContents(Rect inRect)
+    {
+        base.DoSettingsWindowContents(inRect);
+        settings.DoWindowContents(inRect);
+    }
+
+    public override string SettingsCategory()
+    {
+        return "BattleChinchilla_SettingsCategory".Translate();
+    }
+}
