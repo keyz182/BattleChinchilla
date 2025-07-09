99976041d98ba63e346261e30472bcacd895e5f7
Kieran David Evans <keyz182@gmail.com>
2025-07-09 09:09:56 +0100

    fix core not equipping

--- 1.5/Source/BattleChinchilla.EXOSuit/Building_MaintenanceBay_Patch.cs
+++ 1.5/Source/BattleChinchilla.EXOSuit/Building_MaintenanceBay_Patch.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using HarmonyLib;
 using RimWorld;
@@ -10,17 +11,19 @@ namespace BattleChinchilla.EXOSuit;
 [HarmonyPatch(typeof(Building_MaintenanceBay))]
 public static class Building_MaintenanceBay_Patch
 {
+    public static Lazy<ThingDef> Core = new(() => ThingDef.Named("BC_Apparel_Mechachilla_Core") );
+
     [HarmonyPatch(nameof(Building_MaintenanceBay.ModuleStorage), MethodType.Getter)]
     [HarmonyPostfix]
     public static void ModuleStorage_Getter_Patch(Building_MaintenanceBay __instance, ref List<Apparel> __result)
     {
         if (__instance is Building_MechachillaBay)
         {
-            __result.RemoveAll(app => app is WalkerGear.WalkerGear_Core && app.def != BattleChinchillaDefOf.BC_ChinchillaBattleArmour);
+            __result.RemoveAll(app => app is WalkerGear_Core && app.def != Core.Value);
         }
         else
         {
-            __result.RemoveAll(app => app.def == BattleChinchillaDefOf.BC_ChinchillaBattleArmour);
+            __result.RemoveAll(app => app.def == Core.Value);
         }
     }
 
